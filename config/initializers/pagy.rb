07e73aa9c24f4f77adb926be93aea4692feaa9b5
dailson-igo <dailson.araujo@tce.ap.gov.br>
2025-02-18 13:24:34 -0300

    Adjust pagination with numbers

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -7,3 +7,33 @@ Pagy::DEFAULT[:size]  = 7 # Navbar links
 # default :empty_page (other options :last_page and :exception )
 require "pagy/extras/overflow"
 Pagy::DEFAULT[:overflow] = :last_page
+
+# Customiza a geração das séries para não incluir o :gap à esquerda e a última página
+# https://www.beflagrant.com/blog/a-pagination-story-2024-07-15
+class Pagy
+  def custom_series(size: @vars[:size], **_)
+    half_size = (size / 2)
+
+    # assume the current page will be in the middle and go half down, but not below 1
+    left = [ @page - half_size, 1 ].max
+    # now go fully up from there, but not above the last page
+    right = [ left + size, @last ].min
+    # and now readjust the first number if the last had to be capped
+    left = [ [ left, right - size ].min, 1 ].max
+
+    result = [
+      (left...@page).to_a,
+      @page.to_s,
+      ((@page + 1)..right).to_a
+    ].flatten
+
+    if right != @pages
+      result[-1] = :gap
+    end
+
+    result
+  end
+
+  # Sobrescreve o método series padrão para usar o custom_series
+  # alias_method :series, :custom_series
+end
